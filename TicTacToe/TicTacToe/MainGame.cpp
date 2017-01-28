8706e4650dacb5d9b7b6fdda8f623753e56df1a4
Ashwin P G <pgashwin@gmail.com>
2017-01-28 23:49:00 +0530

    Added an array
    And added some logic

--- TicTacToe/TicTacToe/MainGame.cpp
+++ TicTacToe/TicTacToe/MainGame.cpp
@@ -2,24 +2,28 @@
 
 void TicTacToe::PrintGameScreen() 
 {
+	grid[0] = 1;
+	cout << grid[0]<<endl;
 }
 
 void TicTacToe::WelcomeScreen()
 {
 	cout << "TIC TAC TOE" << endl;
 	cout << "Player 1 - X \nPlayer 2 - O" << endl;
+	PrintGameScreen();
+
 }
 
 int TicTacToe::GameLogic()
 {
 
+	return 0;
 }
 
 int main()
 {
 	TicTacToe myGame;
 	myGame.WelcomeScreen();
-	myGame.PrintGameScreen();
 	system("pause");
 	return 0;
 }
