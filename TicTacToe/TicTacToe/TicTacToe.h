610b76605ed092816324ab901ec18dfd7d76def7
Ashwin P G <pgashwin@gmail.com>
2017-01-29 14:04:53 +0530

    Minor

--- TicTacToe/TicTacToe/TicTacToe.h
+++ TicTacToe/TicTacToe/TicTacToe.h
@@ -1,13 +1,16 @@
 #pragma once
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 class TicTacToe
 {
 public:
+	int grid[3]	;
 	void PrintGameScreen();
 	void WelcomeScreen();
+	int GameLogic();
 
 };
 
