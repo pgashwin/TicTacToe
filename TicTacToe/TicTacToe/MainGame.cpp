ebde356336f41deaf937e9caad33a5673506038f
Ashwin P G <pgashwin@gmail.com>
2017-02-02 00:20:42 +0530

    Finished with bugs
    FIX BUGS

--- TicTacToe/TicTacToe/MainGame.cpp
+++ TicTacToe/TicTacToe/MainGame.cpp
@@ -22,45 +22,145 @@ void TicTacToe::WelcomeScreen()
 
 
 
-void TicTacToe::PrintGameScreen()
+int TicTacToe::PrintGameScreen()
 {
-	
-	//to display all the contents in the array
-	for (int i = 0; i < height; i++)
+	if (InputCounter != 9)
 	{
-		for (int j = 0; j < width; j++)
+		//to display all the contents in the array
+		for (int i = 0; i < height; i++)
 		{
-			cout << grid[i][j] << ' ';
+			for (int j = 0; j < width; j++)
+			{
+				cout << grid[i][j] << ' ';
+			}
+			cout << endl;
 		}
-		cout << endl;
+		if (GameOver == false)
+			RecieveInput();
 	}
-	RecieveInput();
+	else
+	{
+		cout << "Match Draw" << endl;
+	}
+	return 0;
 }
 
 
 //Take input from the user
 void TicTacToe::RecieveInput() {
 	int input;
+
 	cin >> input;
-	AssignValues(1);
+	if (IsPlayer1==true){
+		grid_value = "X";
+	}
+	else
+	{
+		grid_value = "O";
+	}
+	AssignValues(input);
 }
+
 
 int TicTacToe::AssignValues(int grid_number) {
 	switch (grid_number) {
-	case 1: grid[0][0] = grid_value;
-		cout << "Cell 1 selected"<<endl;
+	case 1:  grid[0][0] = grid_value; 
+		break;
+	case 2: grid[0][1] = grid_value;
+		break;
+	case 3: grid[0][2] = grid_value;
+		break;
+	case 4: grid[1][0] = grid_value;
+		break;
+	case 5: grid[1][1] = grid_value;
+		break;
+	case 6: grid[1][2] = grid_value;
+		break;
+	case 7: grid[2][0] = grid_value;
+		break;
+	case 8: grid[2][1] = grid_value;
+		break;
+	case 9: grid[2][2] = grid_value;
+		break;
 
-	default:cout<<grid_number;
+	default: cout << "Enter a Valid input" << endl;
+		break;
 	}
-	PrintGameScreen();
-	return 0;
-}
 
-int TicTacToe::GameLogic()
-{
+	
+
+	//switch the player after every input
+	if (IsPlayer1 == false) {
+		IsPlayer1 = true;
+	}
+	else
+	{
+		IsPlayer1 = false;
+	}
+
+	InputCounter++; //increment counter after every input;
+
+	GameLogic();
 	
 	return 0;
 }
+
+int TicTacToe::GameLogic() //checking the grid values
+{
+		if (InputCounter < 3) {
+			PrintGameScreen();
+			return 0;
+		}
+		else
+		{
+			if (grid[0][0] == grid[0][1] && grid[0][0] == grid[0][2])
+			{
+				PrintWinner();
+			}
+			else if (grid[0][0] == grid[1][0] && grid[0][0] == grid[2][0])
+			{
+				PrintWinner();
+			}
+			else if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2])
+			{
+				PrintWinner();
+			}
+			else if (grid[0][1] == grid[1][1] && grid[1][1] == grid[2][1])
+			{
+				PrintWinner();
+			}
+			else if (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0])
+			{
+				PrintWinner();
+			}
+			else if (grid[0][2] == grid[1][2] && grid[1][2] == grid[2][2])
+			{
+				PrintWinner();
+			}
+			else
+			{
+				PrintGameScreen();
+			}
+
+		}
+	return 0;
+}
+
+void TicTacToe::PrintWinner() {
+
+	GameOver = true;
+	if (IsPlayer1 == false) {
+		cout << "Player 1 Wins" << endl;
+		PrintGameScreen();
+	}
+	else
+	{
+		cout << "Player 2 Wins" << endl;
+		PrintGameScreen();
+	}
+	
+}
+
 
 int main()
 {
--- TicTacToe/TicTacToe/TicTacToe.h
+++ TicTacToe/TicTacToe/TicTacToe.h
@@ -10,15 +10,18 @@ public:
 	int height = 3, width = 3;								//row and column
 	string grid[3][3];
 	//int grid_number; // value selected by the user
-	string grid_value = "X"; //Holds x and o
+	string grid_value; //Holds x and o
 	bool HasGameBegan;
+	bool IsPlayer1 = true; //to check if it's player 1 or not
+	int InputCounter=0;
+	bool GameOver = false;
 
 	void WelcomeScreen();
-	void PrintGameScreen();
+	int PrintGameScreen();
 	void RecieveInput(); 
 	int AssignValues(int grid_number);
 	int GameLogic();
-
+	void PrintWinner(); //Print the winner
 };
 	
 
